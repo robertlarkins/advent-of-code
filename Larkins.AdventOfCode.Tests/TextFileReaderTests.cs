cf13f1ab7e293dfa590955195d7ff1a047d67b1f
Robert Larkins <ayb4btu@gmail.com>
2024-11-29 22:36:39 +1300

    Add solution with src and test projects
    This adds the base solution and projects. It also includes an initial
    text file reader.

---
+++ Larkins.AdventOfCode.Tests/TextFileReaderTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+
+namespace Larkins.AdventOfCode.Tests;
+
+public class TextFileReaderTests
+{
+    /// <summary>
+    /// While possible to use TestableIO (https://github.com/TestableIO/System.IO.Abstractions)
+    /// Just use a real file initially.
+    /// </summary>
+    [Fact]
+    public void Text_can_be_read_from_specified_file()
+    {
+        var reader = new TextFileReader();
+        
+        var result = reader.ReadAllTextInFile("some-text-file.txt");
+
+        var expected = """
+           Some text on a line
+           another line
+           
+           one more.
+           
+           """;
+
+        result.Should().Be(expected);
+    }
+}
---
+++ src/Larkins.AdventOfCode/TextFileReader.cs
@@ -0,0 +1,48 @@
+using System.Text;
+
+namespace Larkins.AdventOfCode;
+
+public class TextFileReader
+{
+    public string ReadAllTextInFile(string filename)
+    {
+        return File.ReadAllText(filename);
+    }
+
+    /// <summary>
+    /// Implementation based on: https://learn.microsoft.com/en-us/troubleshoot/developer/visualstudio/csharp/language-compilers/read-write-text-file
+    /// </summary>
+    /// <param name="filePath"></param>
+    /// <returns></returns>
+    public string ReadFileByLine(string filePath)
+    {
+        var stringBuilder = new StringBuilder();
+        try
+        {
+            //Pass the file path and file name to the StreamReader constructor
+            StreamReader sr = new StreamReader(filePath);
+            //Read the first line of text
+            var line = sr.ReadLine();
+            //Continue to read until you reach end of file
+            while (line != null)
+            {
+                //write the line to console window
+                stringBuilder.AppendLine(line);
+                //Read the next line
+                line = sr.ReadLine();
+            }
+            //close the file
+            sr.Close();
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Executing finally block.");
+        }
+        
+        return stringBuilder.ToString();
+    }
+}
