493be678cd733a329d7a7abecf0be31637f27047
Robert Larkins <ayb4btu@gmail.com>
2024-12-04 17:46:19 +1300

    Refactor 2024 Day03 Part02 Solver
    This is to simplify the regex, and help make it more readable.

--- src/Larkins.AdventOfCode/AdventOfCode2024/Day03/Year2024Day03Part02Solver.cs
+++ src/Larkins.AdventOfCode/AdventOfCode2024/Day03/Year2024Day03Part02Solver.cs
@@ -7,7 +7,7 @@ public class Year2024Day03Part02Solver
     public int Solve(string input)
     {
         var dontFollowedByDo = @"don't\(\).*?do\(\)";
-        var dontNotFollowedByDo = @"don't\(\)(?!.*don't\(\)).*$"; // Negative look ahead to find the last don't
+        var dontNotFollowedByDo = @"don't\(\).*$";
         
         input = Regex.Replace(input, dontFollowedByDo, "", RegexOptions.Singleline);
         input = Regex.Replace(input, dontNotFollowedByDo, "", RegexOptions.Singleline);
@@ -17,15 +17,15 @@ public class Year2024Day03Part02Solver
 
     private int ExtractMultiplications(string input)
     {
-        var pattern = @"mul\((\d{1,3}),(\d{1,3})\)";
+        var pattern = @"mul\((?<num1>\d{1,3}),(?<num2>\d{1,3})\)";
         var patternMatches = Regex.Matches(input, pattern);
 
         var total = 0;
         
         foreach (Match match in patternMatches)
         {
-            var number1 = int.Parse(match.Groups[1].Value);
-            var number2 = int.Parse(match.Groups[2].Value);
+            var number1 = int.Parse(match.Groups["num1"].Value);
+            var number2 = int.Parse(match.Groups["num2"].Value);
             
             total += number1 * number2;
         }
