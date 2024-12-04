7a686f5bb9b43f1d05b42b7ad3de7b285d9860aa
Robert Larkins <ayb4btu@gmail.com>
2024-12-04 21:18:24 +1300

    Add 2024 Day 04 Solvers

---
+++ tests/Larkins.AdventOfCode.Tests/AdventOfCode2024/Day04/Year2024Day04Part01Tests.cs
@@ -0,0 +1,30 @@
+using FluentAssertions;
+using Larkins.AdventOfCode.AdventOfCode2024.Day04;
+
+namespace Larkins.AdventOfCode.Tests.AdventOfCode2024.Day04;
+
+public class Year2024Day04Part01Tests
+{
+    [Fact]
+    public void Day04Part01()
+    {
+        var input = """
+            MMMSXXMASM
+            MSAMXMSMSA
+            AMXSXMAAMM
+            MSAMASMSMX
+            XMASAMXAMM
+            XXAMMXXAMA
+            SMSMSASXSS
+            SAXAMASAAA
+            MAMMMXMMMM
+            MXMXAXMASX
+            """;
+        
+        var inputLines = input.Split(Environment.NewLine);
+        
+        var solver = new Year2024Day04Part01Solver();
+        var result = solver.Solve(inputLines);
+        result.Should().Be(18);
+    }
+}
---
+++ tests/Larkins.AdventOfCode.Tests/AdventOfCode2024/Day04/Year2024Day04Part02Tests.cs
@@ -0,0 +1,30 @@
+using FluentAssertions;
+using Larkins.AdventOfCode.AdventOfCode2024.Day04;
+
+namespace Larkins.AdventOfCode.Tests.AdventOfCode2024.Day04;
+
+public class Year2024Day04Part02Tests
+{
+    [Fact]
+    public void Day04Part02()
+    {
+        var input = """
+                    MMMSXXMASM
+                    MSAMXMSMSA
+                    AMXSXMAAMM
+                    MSAMASMSMX
+                    XMASAMXAMM
+                    XXAMMXXAMA
+                    SMSMSASXSS
+                    SAXAMASAAA
+                    MAMMMXMMMM
+                    MXMXAXMASX
+                    """;
+        
+        var inputLines = input.Split(Environment.NewLine);
+        
+        var solver = new Year2024Day04Part02Solver();
+        var result = solver.Solve(inputLines);
+        result.Should().Be(9);
+    }
+}
