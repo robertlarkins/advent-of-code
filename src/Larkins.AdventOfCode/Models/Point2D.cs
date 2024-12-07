ea70e997643e90fc98895d25b5516ccac60b431d
Robert Larkins <ayb4btu@gmail.com>
2024-12-07 22:13:15 +1300

    Add 2024 Day06 Part01 Solver

---
+++ src/Larkins.AdventOfCode/Models/Point2D.cs
@@ -0,0 +1,3 @@
+namespace Larkins.AdventOfCode.Models;
+
+public record Point2D(int Y, int X);
---
+++ tests/Larkins.AdventOfCode.Tests/GlobalUsings.cs
@@ -0,0 +1,7 @@
+// Global usings can instead be added into the project's csproj file
+// The XUnit test project template references Xunit this way:
+// <ItemGroup>
+//   <Using Include="Xunit"/>
+// </ItemGroup>
+
+global using FluentAssertions;
