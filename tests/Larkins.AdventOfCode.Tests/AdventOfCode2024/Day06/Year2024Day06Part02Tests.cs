031ad3200caa7b5d775cda39ff9c6fd72b45a53f
Robert Larkins <ayb4btu@gmail.com>
2024-12-08 11:30:48 +1300

    Add 2024 Day06 Part02 Solver

---
+++ tests/Larkins.AdventOfCode.Tests/AdventOfCode2024/Day06/Year2024Day06Part02Tests.cs
@@ -0,0 +1,29 @@
+using Larkins.AdventOfCode.AdventOfCode2024.Day06GuardGallivant;
+
+namespace Larkins.AdventOfCode.Tests.AdventOfCode2024.Day06;
+
+public class Year2024Day06Part02Tests
+{
+    [Fact]
+    public void Day06Part02()
+    {
+        var input = """
+            ....#.....
+            .........#
+            ..........
+            ..#.......
+            .......#..
+            ..........
+            .#..^.....
+            ........#.
+            #.........
+            ......#...
+            """;
+        
+        var inputLines = input.Split(Environment.NewLine);
+        
+        var solver = new Year2024Day06Part02Solver();
+        var result = solver.Solve(inputLines);
+        result.Should().Be(6);
+    }
+}
