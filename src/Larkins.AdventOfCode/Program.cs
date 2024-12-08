8dc690514008586dcc6962f4c85a25546568eee3
Robert Larkins <ayb4btu@gmail.com>
2024-12-08 15:55:58 +1300

    Add missing program.cs changes

--- src/Larkins.AdventOfCode/Program.cs
+++ src/Larkins.AdventOfCode/Program.cs
@@ -1,3 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Larkins.AdventOfCode.AdventOfCode2024.Day06GuardGallivant;
+using Larkins.AdventOfCode.Utilities;
+
+BenchmarkRunner.Run<BenchmarkClass>();
+
+[MemoryDiagnoser]
+public class BenchmarkClass
+{
+    private IEnumerable<string> input;
+    
+    public BenchmarkClass()
+    {
+        var inputFilePath = @"";
+        var reader = new TextFileReader();
+        input = reader.ReadTextToSeparateLines(inputFilePath);
+    }
+    
+    [Benchmark]
+    public void RunPart1()
+    {
+        var solver = new Year2024Day06Part01Solver(input);
+        solver.Solve();
+    }
+    
+    [Benchmark]
+    public void RunPart2()
+    {
+        var solver = new Year2024Day06Part02Solver(input);
+        solver.Solve();
+    }
+}
