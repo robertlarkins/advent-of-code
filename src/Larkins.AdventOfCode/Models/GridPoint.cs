4dd99ad3db55578202d4506b79cf37c50dea8a4e
Robert Larkins <ayb4btu@gmail.com>
2024-12-10 22:30:33 +1300

    Add 2024 Day10 Solvers

--- src/Larkins.AdventOfCode/Models/GridPoint.cs
+++ src/Larkins.AdventOfCode/Models/GridPoint.cs
@@ -1,3 +1,14 @@
 namespace Larkins.AdventOfCode.Models;
 
-public record GridPoint(int Row, int Col);
+public record GridPoint(
+    int Row,
+    int Col)
+{
+    public bool IsWithinGrid(int height, int width)
+    {
+        var isOnValidRow = Row >= 0 && Row < height;
+        var isOnValidCol = Col >= 0 && Col < width;
+        
+        return isOnValidRow && isOnValidCol;
+    }
+}
