4111d7caf26eddd35f2be3f6daed9d6f02f37abe
Robert Larkins <ayb4btu@gmail.com>
2024-12-15 16:04:05 +1300

    Add 2024 Day07 Solvers

---
+++ src/Larkins.AdventOfCode/AdventOfCode2024/Day07BridgeRepair/Year2024Day07Part01Solver.cs
@@ -0,0 +1,71 @@
+namespace Larkins.AdventOfCode.AdventOfCode2024.Day07BridgeRepair;
+
+public class Year2024Day07Part01Solver
+{
+    private readonly List<Equation> equations;
+
+    public Year2024Day07Part01Solver(IEnumerable<string> input)
+    {
+        equations = ParseInput(input.ToList());
+    }
+
+    public long Solve()
+    {
+        return equations.Where(IsValidEquation).Sum(eq => eq.TestValue);
+    }
+
+    private bool IsValidEquation(Equation equation)
+    {
+        var testValue = equation.TestValue;
+        var numbers = equation.Numbers;
+
+        var set = new HashSet<long>
+        {
+            numbers[0]
+        };
+
+        foreach (var number in numbers.Skip(1))
+        {
+            var set2 = new HashSet<long>();
+
+            foreach (var valueSoFar in set)
+            {
+                var newNum1 = valueSoFar * number;
+                var newNum2 = valueSoFar + number;
+
+                if (newNum1 <= testValue)
+                {
+                    set2.Add(newNum1);
+                }
+
+                if (newNum2 <= testValue)
+                {
+                    set2.Add(newNum2);
+                }
+            }
+
+            set = set2;
+        }
+
+        return set.Count(results => results == testValue) >= 1;
+    }
+
+    private List<Equation> ParseInput(List<string> input)
+    {
+        var equations = new List<Equation>();
+
+        foreach (var line in input)
+        {
+            var parts = line.Split(": ");
+            var testValue = long.Parse(parts[0]);
+            var numbers = parts[1].Split(" ").Select(long.Parse).ToList();
+
+            var equation = new Equation(testValue, numbers);
+            equations.Add(equation);
+        }
+
+        return equations;
+    }
+
+    private record Equation(long TestValue, List<long> Numbers);
+}
---
+++ src/Larkins.AdventOfCode/Extensions/LongExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Larkins.AdventOfCode.Extensions;
+
+public static class LongExtensions
+{
+    public static int NumberOfDigits(this long number) =>
+        (int)Math.Floor(Math.Log10(number) + 1);
+}
