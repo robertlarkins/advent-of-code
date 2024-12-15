37890e2ada2c1a0f9190d8fe52ec246f52e4e6e5
Robert Larkins <ayb4btu@gmail.com>
2024-12-15 16:04:05 +1300

    Add 2024 Day07 Solvers

---
+++ tests/Larkins.AdventOfCode.Tests/AdventOfCode2024/Day07BridgeRepair/Year2024Day07Part01Tests.cs
@@ -0,0 +1,28 @@
+using Larkins.AdventOfCode.AdventOfCode2024.Day07BridgeRepair;
+
+namespace Larkins.AdventOfCode.Tests.AdventOfCode2024.Day07BridgeRepair;
+
+public class Year2024Day07Part01Tests
+{
+    [Fact]
+    public void Day07Part01()
+    {
+        var input = """
+                    190: 10 19
+                    3267: 81 40 27
+                    83: 17 5
+                    156: 15 6
+                    7290: 6 8 6 15
+                    161011: 16 10 13
+                    192: 17 8 14
+                    21037: 9 7 18 13
+                    292: 11 6 16 20
+                    """;
+
+        var inputLines = input.Split(Environment.NewLine);
+
+        var solver = new Year2024Day07Part01Solver(inputLines);
+        var result = solver.Solve();
+        result.Should().Be(3749);
+    }
+}
