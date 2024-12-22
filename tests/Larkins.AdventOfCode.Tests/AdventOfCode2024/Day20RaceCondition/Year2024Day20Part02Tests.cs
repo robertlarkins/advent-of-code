17c3b91ed9fbb448d14ced0fccd15635504a67a7
Robert Larkins <ayb4btu@gmail.com>
2024-12-22 23:02:13 +1300

    Improve 2024 Day20 Part02 Solver
     This adds more tests to ensure the position and new position values are
     correctly calculated. It was found 2 was the best value, which is the
     shortest distance to get through a wall.

--- tests/Larkins.AdventOfCode.Tests/AdventOfCode2024/Day20RaceCondition/Year2024Day20Part02Tests.cs
+++ tests/Larkins.AdventOfCode.Tests/AdventOfCode2024/Day20RaceCondition/Year2024Day20Part02Tests.cs
@@ -1,4 +1,3 @@
-using Larkins.AdventOfCode.AdventOfCode2024.Day19LinenLayout;
 using Larkins.AdventOfCode.AdventOfCode2024.Day20RaceCondition;
 
 namespace Larkins.AdventOfCode.Tests.AdventOfCode2024.Day20RaceCondition;
@@ -31,5 +30,85 @@ public class Year2024Day20Part02Tests
         var solver = new Year2024Day20Part02Solver(inputLines, 76, 20); // minimum savings
         var result = solver.Solve();
         result.Should().Be(3);
+    }
+
+    [Theory]
+    [InlineData(2, 2, 3)]
+    [InlineData(2, 3, 7)]
+    [InlineData(2, 4, 9)]
+    [InlineData(3, 2, 2)]
+    [InlineData(3, 3, 4)]
+    [InlineData(3, 4, 4)]
+    [InlineData(4, 3, 4)]
+    [InlineData(4, 4, 4)]
+    [InlineData(5, 2, 1)]
+    [InlineData(5, 3, 1)]
+    [InlineData(6, 2, 1)]
+    public void Day20Part02_example4(int minimumSavings, int cheatLength, int expected)
+    {
+        var input = """
+                    ######
+                    #S...#
+                    ####.#
+                    #E...#
+                    ######
+                    """;
+
+        var inputLines = input.Split(Environment.NewLine);
+
+        var solver = new Year2024Day20Part02Solver(inputLines, minimumSavings, cheatLength);
+        var result = solver.Solve();
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(2, 2, 2)]
+    [InlineData(2, 3, 5)]
+    [InlineData(3, 3, 2)]
+    [InlineData(3, 4, 3)]
+    [InlineData(4, 3, 2)]
+    [InlineData(4, 4, 3)]
+    [InlineData(4, 5, 4)]
+    public void Day20Part02_example2(int minimumSavings, int cheatLength, int expected)
+    {
+        var input = """
+                    ########
+                    #S.....#
+                    ######.#
+                    ####E..#
+                    ########
+                    """;
+
+        var inputLines = input.Split(Environment.NewLine);
+
+        var solver = new Year2024Day20Part02Solver(inputLines, minimumSavings, cheatLength);
+        var result = solver.Solve();
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(2, 2, 0)]
+    [InlineData(2, 3, 2)]
+    [InlineData(3, 3, 1)]
+    [InlineData(3, 4, 2)]
+    [InlineData(4, 3, 1)]
+    [InlineData(4, 4, 2)]
+    [InlineData(4, 5, 3)]
+    public void Day20Part02_example3(int minimumSavings, int cheatLength, int expected)
+    {
+        var input = """
+                    ########
+                    #S.....#
+                    ######.#
+                    ######.#
+                    ####E..#
+                    ########
+                    """;
+
+        var inputLines = input.Split(Environment.NewLine);
+
+        var solver = new Year2024Day20Part02Solver(inputLines, minimumSavings, cheatLength);
+        var result = solver.Solve();
+        result.Should().Be(expected);
     }
 }
