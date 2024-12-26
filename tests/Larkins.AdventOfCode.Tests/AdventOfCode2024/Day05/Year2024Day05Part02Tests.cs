e19dda1efcc21220cca66cb4296261448e2f94d8
Robert Larkins <ayb4btu@gmail.com>
2024-12-26 23:08:35 +1300

    add 2024 Day05 Part02 Solver

---
+++ tests/Larkins.AdventOfCode.Tests/AdventOfCode2024/Day05/Year2024Day05Part02Tests.cs
@@ -0,0 +1,47 @@
+using Larkins.AdventOfCode.AdventOfCode2024.Day05;
+
+namespace Larkins.AdventOfCode.Tests.AdventOfCode2024.Day05;
+
+public class Year2024Day05Part02Tests
+{
+    [Fact]
+    public void Day05Part02()
+    {
+        var input = """
+                    47|53
+                    97|13
+                    97|61
+                    97|47
+                    75|29
+                    61|13
+                    75|53
+                    29|13
+                    97|29
+                    53|29
+                    61|53
+                    97|53
+                    61|29
+                    47|13
+                    75|47
+                    97|75
+                    47|61
+                    75|61
+                    47|29
+                    75|13
+                    53|13
+
+                    75,47,61,53,29
+                    97,61,53,29,13
+                    75,29,13
+                    75,97,47,61,53
+                    61,13,29
+                    97,13,75,29,47
+                    """;
+
+        var inputLines = input.Split(Environment.NewLine);
+
+        var solver = new Year2024Day05Part02Solver(inputLines);
+        var result = solver.Solve();
+        result.Should().Be(123);
+    }
+}
