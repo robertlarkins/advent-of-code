28db6c7203d214f1c48d3b9f2742a4129ee8b9b2
Robert Larkins <ayb4btu@gmail.com>
2025-12-05 05:07:38 +1300

    Add 2025 Day 4 Part 1 Solver

---
+++ src/Larkins.AdventOfCode/AdventOfCode2025/Day04PrintingDepartment/Year2025Day04Part01Solver.cs
@@ -0,0 +1,28 @@
+﻿namespace Larkins.AdventOfCode.AdventOfCode2025.Day04PrintingDepartment;
+
+public class Year2025Day04Part01Solver(IEnumerable<string> input)
+{
+    private readonly List<string> inputLines = input.ToList();
+
+    public int Solve()
+    {
+        var total = 0;
+
+
+
+
+        return total;
+    }
+
+    private char[,] ConvertInput()
+    {
+        var grid = new char[inputLines.Count, inputLines[0].Length];
+
+        for (var line = 0; line < inputLines.Count; line++)
+        {
+            grid[line, ..] = inputLines[line];
+        }
+
+    }
+
+}
