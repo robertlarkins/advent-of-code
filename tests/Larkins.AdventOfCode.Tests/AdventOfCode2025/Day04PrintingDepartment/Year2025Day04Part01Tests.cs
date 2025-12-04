9d4ea2a3d4be252dc0283b5c289979aff4bfa18d
Robert Larkins <ayb4btu@gmail.com>
2025-12-05 05:07:38 +1300

    Add 2025 Day 4 Part 1 Solver

---
+++ tests/Larkins.AdventOfCode.Tests/AdventOfCode2025/Day04PrintingDepartment/Year2025Day04Part01Tests.cs
@@ -0,0 +1,30 @@
+﻿using Larkins.AdventOfCode.AdventOfCode2025.Day04PrintingDepartment;
+
+namespace Larkins.AdventOfCode.Tests.AdventOfCode2025.Day04PrintingDepartment;
+
+public class Year2025Day04Part01Tests
+{
+    [Fact]
+    public void Paper_is_accessible_if_fewer_than_four_rolls_around_ir()
+    {
+        var input = """
+            ..@@.@@@@.
+            @@@.@.@.@@
+            @@@@@.@.@@
+            @.@@@@..@.
+            @@.@@@@.@@
+            .@@@@@@@.@
+            .@.@.@.@@@
+            @.@@@.@@@@
+            .@@@@@@@@.
+            @.@.@@@.@.
+            """;
+
+        var inputLines = input.Split(Environment.NewLine);
+        var solver = new Year2025Day04Part01Solver(inputLines);
+
+        var result = solver.Solve();
+
+        result.Should().Be(13);
+    }
+}
